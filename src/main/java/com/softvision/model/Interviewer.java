c1d2c5cff2f4359c3dac59983a6bb5bac02a7782
Jagadeeshwar Karnam <jagadeeshwar.karnam@SPI.COM>
2018-06-13 20:28:53 +0530

    Merged Code

--- src/main/java/com/softvision/model/Interviewer.java
+++ src/main/java/com/softvision/model/Interviewer.java
@@ -1,6 +1,12 @@
 package com.softvision.model;
 
+import com.softvision.helper.LocalDateTimeAttributeConverter;
+import java.io.Serializable;
+import java.time.LocalDateTime;
+import java.util.Comparator;
 import java.util.List;
+import javax.persistence.Convert;
+import javax.persistence.Converter;
 import javax.persistence.GeneratedValue;
 import javax.validation.constraints.Min;
 import javax.validation.constraints.NotNull;
@@ -12,14 +18,15 @@ import org.springframework.data.mongodb.core.mapping.Document;
 
 @Data
 @Document(collection = "interviewer")
-public class Interviewer implements Comparable<Interviewer> {
+@NotNull
+public class Interviewer implements Comparable<Interviewer>{
 
     @Id
     @GeneratedValue
     private String id;
 
     @NotNull
-    @Min(value = 0, message = "Interviewer Id cannot be null or empty")
+    @Min(value=0,message="Interviewer Id cannot be null or empty")
     private String interviewerID;
 
     @NotNull(message = "Interviewer First Name cannot be null")
@@ -32,20 +39,27 @@ public class Interviewer implements Comparable<Interviewer> {
 
     private String emailId;
 
-    @Pattern(regexp = "(^$|[0-9]{10})", message = "Invalid Phone number")
+    @Pattern(regexp="(^$|[0-9]{10})",message="Invalid Phone number")
     private String contactNumber;
 
-    private List<String> listTechStack;
     private boolean isDeleted;
+
+    @Convert(converter = LocalDateTimeAttributeConverter.class)
+    //@NotNull(message = "Creation date cannot be null")
+    private LocalDateTime createdDate;
+
+    @Convert(converter = LocalDateTimeAttributeConverter.class)
+    //@NotNull(message = "Modified date cannot be null")
+    private LocalDateTime modifiedDate;
+
+    private TechnologyCommunity technologyCommunity;
+
+    private int bandExperience;
 
     @Override
     public int compareTo(Interviewer o) {
-        int val = 0;
-        if (this.getFirstName().compareTo(o.getFirstName()) == -1) {
-            val = -1;
-        } else if (this.getFirstName().compareTo(o.getFirstName()) == 1) {
-            val = 1;
-        }
-        return val;
+        return Comparator.comparing(Interviewer::getFirstName)
+                .thenComparing(Interviewer::getLastName)
+                .compare(this,o);
     }
 }
